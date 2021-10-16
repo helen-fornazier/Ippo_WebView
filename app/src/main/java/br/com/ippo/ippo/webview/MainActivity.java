5fdcd83216f7b81304fbdd3d69726cf57b446468
Helen Koike <helen@koikeco.de>
2021-10-16 14:49:56 -0300

    Push notif FCM and disable cache

--- app/src/main/java/br/com/ippo/ippo/webview/MainActivity.java
+++ app/src/main/java/br/com/ippo/ippo/webview/MainActivity.java
@@ -1,5 +1,8 @@
 package br.com.ippo.ippo.webview;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +13,11 @@ import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
@@ -40,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         WebSettings settings = webView.getSettings();
 
         settings.setUseWideViewPort(true);
-        enableHTML5AppCache(settings);
+        //enableHTML5AppCache(settings);
         webView.loadUrl("https://ippo.com.br/iniciomobile");
 
         webView.setWebViewClient(new WebViewClient() {
@@ -50,5 +58,24 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.webview).setVisibility(View.VISIBLE);
             }
         });
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("koike", "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+                        // czovt8_KTYyIk3Mef75e0n:APA91bFRtKOspG0NF_bI8o0KmewNiPUMvIK67TXHD12JPb8ui9vJFz32718fipnC_w8QZB8yZGXw_cxwf6MgWznUJiMI2EdRgE1vibluRC655xNkOFx_vtwhSswYCuBfb-AQkxCiWklO
+                        // Log and toast
+                        //String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d("koike", token);
+                        //Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
